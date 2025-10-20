bf4a7ad2dcfcd5967c18e219b3c77f88262e7ae7
pranavdhardwivedi2006 <pranavdhardwivedi2006@gmail.com>
2025-10-20 22:18:46 +0530

    20/10/2025

---
+++ POTD/2025/october/final_value_of_variable_after_performing_operations.cpp
@@ -0,0 +1,11 @@
+class Solution {
+public:
+    int finalValueAfterOperations(vector<string>& operations) {
+        int ans = 0;
+        for(auto& it : operations) {
+            if(it[1] == '+') ans++;
+            else ans--;
+        }
+        return ans;
+    }
+};
