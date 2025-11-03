f06f6dae6d7154ced6c233a4da5403c4d349f66a
pranavdhardwivedi2006 <pranavdhardwivedi2006@gmail.com>
2025-11-03 11:12:12 +0530

    03/11/2025

---
+++ POTD/2025/november/minimum_time_to_make_rope_colourful.cpp
@@ -0,0 +1,21 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+class Solution {
+public:
+    int minCost(string colors, vector<int>& neededTime) {
+        int i = 0, n = colors.size();
+        int ans = 0;
+        while(i < n - 1) {
+            char c = colors[i];
+            int maxi = 0, sum = 0;
+            while(colors[i] == c) {
+                maxi = max(maxi, neededTime[i]);
+                sum += neededTime[i];
+                i++;
+            }
+            ans += sum - maxi;
+        }
+        return ans;
+    }
+};
