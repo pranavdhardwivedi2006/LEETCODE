698f6fa39a83463dddab0bea5e2a3ab219f377cd
pranavdhardwivedi2006 <pranavdhardwivedi2006@gmail.com>
2025-12-13 02:12:23 +0530

    04/12/2025

---
+++ POTD/2025/december/4.count_colliosions_on_a_road.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    int countCollisions(string directions) {
+        int n = directions.size(), l = 0, r = n - 1;
+        while(l < n && directions[l] == 'L') l++;
+        while(r >= 0 && directions[r] == 'R') r--;
+        int colls = 0;
+        for(int i = l; i <= r; i++) {
+            if(directions[i] != 'S') colls++;
+        }
+        return colls;
+    }
+};
