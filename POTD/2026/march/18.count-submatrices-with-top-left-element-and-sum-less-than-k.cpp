9c3d048de5ffbff3a1ab1d850459cd45dc4f0e41
pranavdhardwivedi2006 <pranavdhardwivedi2006@gmail.com>
2026-03-18 23:03:48 +0530

    18/03/2026

---
+++ POTD/2026/march/18.count-submatrices-with-top-left-element-and-sum-less-than-k.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    int countSubmatrices(vector<vector<int>>& grid, int k) {
+        int n = grid.size(), m = grid[0].size(), ans = 0;
+        for(int i = 0; i < n; i++) {
+            for(int j = 0; j < m; j++) {
+                grid[i][j] += i ? grid[i-1][j] : 0;
+                grid[i][j] += j ? grid[i][j-1] : 0;
+                grid[i][j] -= i && j ? grid[i-1][j-1] : 0;
+                if(grid[i][j] <= k) ans++;
+            }
+        }
+        return ans;
+    }
+};
