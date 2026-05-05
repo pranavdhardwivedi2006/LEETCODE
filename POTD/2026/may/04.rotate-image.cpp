b3fcbb7e1ec6c89c5dd510ec1ae07acd1e2af5f8
pranavdhardwivedi2006 <pranavdhardwivedi2006@gmail.com>
2026-05-05 11:20:57 +0530

    05/05/2026

---
+++ POTD/2026/may/04.rotate-image.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    void rotate(vector<vector<int>>& matrix) {
+        int n = matrix.size();
+        for (int i = 0; i < n; i++) {
+            for (int j = i; j < n; j++) {
+                swap(matrix[i][j], matrix[j][i]);
+            }
+        }
+        int l = 0, r = n - 1;
+        while (l < r) {
+            for (int i = 0; i < n; i++) {
+                swap(matrix[i][l], matrix[i][r]);
+            }
+            l++;
+            r--;
+        }
+    }
+};
